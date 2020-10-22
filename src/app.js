68a8e89284220551849865c5566413faafcad893
Vento <vento.michael0705@hotmail.com>
2020-10-22 10:35:45 +0200

    add todo validation

--- src/app.js
+++ src/app.js
@@ -3,5 +3,14 @@ import 'alpinejs'
 window.data = function () {
 	return {
 		todos:[],
+		newTodoTitle:null,
+		addTodo(){
+			this.todos.push({
+				id:Date.now(),
+				title:this.newTodoTitle,
+				completed:false
+			})
+			console.log(this.todos)
+		}
 	}
 }
