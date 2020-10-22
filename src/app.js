670e58bc2036369d92d38372e09741ea9fbf4409
Vento <vento.michael0705@hotmail.com>
2020-10-22 11:10:45 +0200

    fix add empty todo

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ import 'alpinejs'
 
 window.data = function () {
 	return {
-		todos:[],
-		newTodoTitle:'',
-		addTodo(){
-			this.todos.push({
-				id:Date.now(),
-				title:this.newTodoTitle,
-				completed:false
-			})
-			this.newTodoTitle = ''
+		todos: [],
+		newTodoTitle: '',
+		addTodo() {
+			if (this.newTodoTitle.trim()) {
+				this.todos.push({
+					id: Date.now(),
+					title: this.newTodoTitle,
+					completed: false
+				})
+				this.newTodoTitle = ''
+			}
 		}
 	}
 }
